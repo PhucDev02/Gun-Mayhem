97178dd15275ba837c06938aad027f266ff38ce7
PhucDev02 <76049784+PhucDev02@users.noreply.github.com>
2024-10-29 00:55:01 +0700

    -modify physic, add indicator

--- Assets/Scripts/Common/ConstValue.cs
+++ Assets/Scripts/Common/ConstValue.cs
@@ -3,16 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.Utilities;
 
+
 public class ConstValue
 {
-    
-    public const float groundRadiusCheck= 0.25f;
     public const int maxLives = 3;
-    public const int moveSpeed = 4;
-    public const float jumpForce = 15;
-    public const float dashTime = 0.25f;
-    public const float dashCoolDownTime = 1f;
-    public const float attackCooldown=.5f;
     public static Vector2 environmentLimitX = new Vector2(-10, 10);
 
 
